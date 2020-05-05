1bb6bbad929335bc7f3f8c571076cbc5f4c23a6f
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-05-05 16:24:53 -0300

    using react-native-flash-message library

--- user-mobile/src/pages/RegisterInfo/index.js
+++ user-mobile/src/pages/RegisterInfo/index.js
@@ -4,6 +4,7 @@ import {
   Image, 
   Alert
 } from 'react-native';
+import {showMessage, hideMessage} from 'react-native-flash-message'
 
 
 
@@ -11,12 +12,22 @@ import styles from './styles'
 
 export default function RegisterInfo() {
   useEffect(()=>{
-    Alert.alert('INFO'  ,'Id Eletrônico se encontra em uma conta do SAAE na área destacada na imagem.')
+    // Alert.alert('INFO'  ,'Id Eletrônico se encontra em uma conta do SAAE na área destacada na imagem.')
+    showMessage({
+      message: 'INFO',
+      description: 'Id Eletrônico se encontra em uma conta do SAAE na área destacada em vermelho na imagem.',
+      type: 'info',
+      titleStyle: { fontWeight: 'bold', fontSize: 20},
+      textStyle: {fontSize: 15},
+      backgroundColor: '#22262e',
+      color: '#FFF',
+      duration: 2500
+    });
   }, [])
 
   return(
     <View style= {styles.background}>
-      <Image  source = {require('../../assets/CONTA.png')}/>
+      <Image source = {require('../../assets/CONTA.png')}/>
     </View>
 
   )
