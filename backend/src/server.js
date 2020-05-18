fbd9c937e52f592826fd203c818381d98f50a77e
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-05-18 16:33:00 -0300

    Changing API port for post on web

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,9 @@ require('./database');
 
 const app = express();
 
-console.log('[Express] Creating a new server');
+console.log('[Express] Creating a new server on port [3000]');
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(8080);
+app.listen(3000);
