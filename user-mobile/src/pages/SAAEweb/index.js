e6a955d956c6378d4289c6e08b2497f5427c4757
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-06-16 03:04:22 -0300

    auto webview

--- user-mobile/src/pages/SAAEweb/index.js
+++ user-mobile/src/pages/SAAEweb/index.js
@@ -1,33 +1,50 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { ActivityIndicator, AsyncStorage } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function SAAEweb() {
   const [userId, setUserId] = useState('');
+  const [script, setScript] = useState('') 
   const [loading, setLoading] = useState(true);
+  const wvRef = useRef();
   const renderLoading = () => {
     <ActivityIndicator style= {{flex: 1}} animating color = "#004384" size = "large"/>
   }
   useEffect(()=> {
     async function getUserId(){
-    const user = await AsyncStorage.getItem('@SAAEapi:user');
-    const jsonUser = JSON.parse(user);
-    setUserId(jsonUser.idElet)  
+      const user = await AsyncStorage.getItem('@SAAEapi:user');
+      const jsonUser = JSON.parse(user);
+      setUserId(jsonUser.idElet)  
     }
     getUserId()
-  }, []);
-  const jsCode = `document.getElementById('txtcod').value='${userId}'
+    const jsCode = `document.getElementById('txtcod').value='${userId}'
    document.querySelectorAll("input[name=Acessar]")[0].click();
   `
+    setScript(jsCode)
+  }, []);
+
 
   return (
     <WebView
-    startInLoadingState = { true }
-    renderLoading = { renderLoading } 
-    scalesPageToFit= {false}
-    javaScriptEnabled={true}
-    injectedJavaScript={jsCode}
-    source={{uri: 'http://autoatendimento.prosanearinfo.com.br/v5.0/index.php?id=A3AQ33E'}}
+      ref={wvRef}
+      startInLoadingState = { true }
+      renderLoading = { renderLoading } 
+      scalesPageToFit= {false}
+      javaScriptEnabled={true}
+      onNavigationStateChange={navState => {
+        if(navState.url==='http://autoatendimento.prosanearinfo.com.br/v5.0/principal.php'){
+          setScript(`
+            document.querySelectorAll("li[class=debitos]")[0].click()
+          `)
+        }else{
+          const jsCode = `document.getElementById('txtcod').value='${userId}'
+            document.querySelectorAll("input[name=Acessar]")[0].click();
+          `
+          setScript(jsCode)
+        }
+      }}
+      injectedJavaScript={script}
+      source={{uri: 'http://autoatendimento.prosanearinfo.com.br/v5.0/index.php?id=A3AQ33E'}}
   />
   );
 }
