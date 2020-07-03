8a800c5ee612875ef85718c48f35a024965b0a24
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-07-03 17:12:29 -0300

    create admin seed config

--- backend/src/database/seeds/20200619192250-create-admin.js
+++ backend/src/database/seeds/20200619192250-create-admin.js
@@ -1,11 +1,14 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+require ('dotenv').config();
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: (queryInterface, Sequelize) => { 
+    const hash = bcrypt.hashSync(process.env.ADMIN_PASS, 10);
     return queryInterface.bulkInsert('employees', [{
       name: 'admin',
       codFunc: '1',
-      password: 'admin',
+      password: hash,
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
