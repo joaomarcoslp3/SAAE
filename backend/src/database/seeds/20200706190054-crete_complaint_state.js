caff1f4992def7f106cd0be5f5710c01c04f4e4e
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-07-06 19:29:32 -0300

    create "confirmed" complaint state

--- backend/src/database/seeds/20200706190054-crete_complaint_state.js
+++ backend/src/database/seeds/20200706190054-crete_complaint_state.js
@@ -3,20 +3,30 @@
 module.exports = {
   up: (queryInterface, Sequelize) => { 
     return queryInterface.bulkInsert('complaint_state', [{
+      id: 1,
       complaint_state_title: 'New',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
-      complaint_state_title: 'To do',
+      id: 2,
+      complaint_state_title: 'Doing',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
+      id: 3,
       complaint_state_title: 'Done',
       createdAt: new Date(),
       updatedAt: new Date()
-    }]);
+    },
+    {
+      id: 4,
+      complaint_state_title: 'Confirmed',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+  ]);
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('complaint_state', null, {});
