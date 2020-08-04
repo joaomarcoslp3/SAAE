78b7a888ca1c1b2c772199cf44cb75ebd5e2f168
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-08-04 18:09:38 -0300

    adding cors library

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors')
 
 require('./database');
 
@@ -11,5 +12,6 @@ console.log(`[Express] Creating a new server on [${port}]`);
 
 app.use(express.json());
 app.use(routes);
+app.use(cors);
 
 app.listen(port);
