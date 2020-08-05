2311ca8c44b947e121a3cb70b673b57be277f00a
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-08-05 00:27:59 -0300

    api register w/ confirm register page

--- employees-web/src/pages/Register/Register.js
+++ employees-web/src/pages/Register/Register.js
@@ -1,9 +1,9 @@
-import React,  {useState, useEffect} from 'react';
-import api from '../../services/api'; 
-import {useHistory} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import { useHistory } from 'react-router-dom';
 import './styles.css'
 
-export default function Register(){
+export default function Register() {
   const [name, setName] = useState('')
   const [codFunc, setCodFunc] = useState('')
   const [password, setPassword] = useState('');
@@ -11,80 +11,78 @@ export default function Register(){
   const history = useHistory();
 
   useEffect(() => {
-    if(localStorage.getItem('emptoken') === null){
+    if (localStorage.getItem('emptoken') === null) {
       history.push('/')
     }
   }, [history])
 
-  function _register(e){
+  function _register(e) {
     e.preventDefault();
 
-      setCodErrorMsg('Código de Funcionário já utilizado.')
-
-      // api.post('/employees/create', {
-      //   name,
-      //   codFunc,
-      //   password
-      // }).then(res =>{
-      //   setTimeout(() => {
-      //     history.push('/register-complete')
-      //   }, 100);
-      // }).catch(err => {
-      //   if(err.response){
-      //     if(err.response.status === 406){
-      //       setCodErrorMsg('Código de Funcionário já utilizado.')
-      //     }
-      //   }
-      // })
+    api.post('/employees/create', {
+      name,
+      codFunc,
+      password
+    }).then(res => {
+      setTimeout(() => {
+        history.push('/register-complete')
+      }, 100);
+    }).catch(err => {
+      if (err.response) {
+        if (err.response.status === 406) {
+          setCodErrorMsg('Código de Funcionário já utilizado.')
+        }
+      }
+    })
   }
-    return(
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 mt-5 mx-auto">
-            <form noValidate onSubmit={_register}>
-              <h1 className="h3 mb-3 font weight-normal">
-                Cadastre um novo funcionário
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 mt-5 mx-auto">
+          <form noValidate onSubmit={_register}>
+            <h1 className="h3 mb-3 font weight-normal">
+              Cadastre um novo funcionário
               </h1>
-              <div className="form-group">
-                  <label htmlFor="name">Nome do Funcionário:</label>
-                  <br/>
-                  <input
-                    name = "name"
-                    placeholder="Insira o nome do Funcionário"
-                    value={name}
-                    onChange = {e => setName(e.target.value)}
-                   />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="codFunc">Código do Funcionário:</label>
-                  <br/>
-                   <input type="text"
-                    name = "codFunc"
-                    placeholder="Insira o Código do Funcionário"
-                    value={codFunc}
-                    className={codErrorMsg ? "error" : null}
-                    onChange = {e => setCodFunc(e.target.value)}
-                   />
-                   <span>{codErrorMsg}</span>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Senha:</label>
-                  <br/>
-                   <input type="password"
-                    name = "password"
-                    placeholder="Insira a senha do Funcionário"
-                    value={password}
-                    onChange = {e => setPassword(e.target.value)}
-                   />
-                </div>
-                <button 
-                type = "submit"
-                className="button">
-                  Cadastre
+            <div className="form-group">
+              <label htmlFor="name">Nome do Funcionário:</label>
+              <br />
+              <input
+                name="name"
+                placeholder="Insira o nome do Funcionário"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="codFunc">Código do Funcionário:</label>
+              <br />
+              <input type="text"
+                name="codFunc"
+                placeholder="Insira o Código do Funcionário"
+                value={codFunc}
+                className={codErrorMsg ? "error" : null}
+                onChange={e => setCodFunc(e.target.value)}
+              />
+              <span>{codErrorMsg}</span>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Senha:</label>
+              <br />
+              <input type="password"
+                name="password"
+                placeholder="Insira a senha do Funcionário"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="button">
+              Cadastre
                 </button>
-            </form>
-          </div>
+          </form>
         </div>
       </div>
-    )
+    </div>
+  )
 }
